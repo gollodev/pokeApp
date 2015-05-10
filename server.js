e9e6bf0e72bc695280cb0d4eae148f97fbacd5c2
Jose Lopez <gollo_autumn@live.com>
2015-05-10 00:47:26 -0430

    add server with express

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express')
+   , app    = express()
+   , path	= require('path');
+
+app.set('port', process.env.PORT || 3000);
+
+app.use(express.static(path.join(__dirname + '/public')));
+
+app.listen(app.get('port'), function () {
+	console.log(' 127.0.0.1:'+ app.get('port') + ' Serving statics files in public');
+});
